1aea14fd4ac18c558b7dab43e5a210c81fe61481
dianawhalen <whalendianaj@gmail.com>
2023-06-14 16:48:38 -0600

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+
+  taskForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let newTaskDescription = event.target.querySelector("#new-task-description").value;
+    let newTaskItem = document.createElement("li");
+    newTaskItem.innerText = newTaskDescription;
+
+    taskList.appendChild(newTaskItem);
+
+    event.target.reset();
+  });
 });
